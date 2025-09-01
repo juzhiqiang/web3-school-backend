0f796ef3886386bf8ddbc4162633a66d30f6c764
miss you <1020814597@qq.com>
2025-09-01 19:41:15 +0800

    Add Migrations contract for Truffle framework

---
+++ contracts/Migrations.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+/**
+ * @title Migrations
+ * @dev Truffle 框架需要的迁移跟踪合约
+ */
+contract Migrations {
+    address public owner = msg.sender;
+    uint public last_completed_migration;
+
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
+    }
+}
